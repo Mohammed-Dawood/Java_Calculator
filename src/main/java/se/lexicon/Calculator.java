cbc82776a033f5a7d11f203b04998d5c3da9597b
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-06-04 14:36:13 +0200

    Update Calculator Class

--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -2,7 +2,7 @@ package se.lexicon;
 
 public class Calculator {
     public static double performOperation(char operator, double number1, double number2) {
-        double result = 0;
+        double result;
         switch (operator) {
             case '+':
                 result = number1 + number2;
@@ -22,7 +22,6 @@ public class Calculator {
             default:
                 throw new UnsupportedOperationException("Invalid operator: " + operator);
         }
-
         return result;
     }
 }
